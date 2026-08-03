1d05ac66c5fe621c313f74a1e8782fb1c7eebd1a
K <s6603051614011@email.kmutnb.ac.th>
2026-08-03 10:09:21 +0700

    parameter in Event Handler
    parameter in Event Handler

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import {Header, Content, Footer } from './func-components';
-import Calender from './class-components';
+import {Calculator2} from './calculator';
+
 function App() {
   
-  return (
-    <>
-      <Header/>
-      <p><center><Calender/></center></p>
-      <Content/>
-      <Footer/>
-    </>
-  )
+  return <Calculator2/>;
 }
 
 export default App;
